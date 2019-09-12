b72ae0a12ebdbce7ac918cc7d5e61cf4322d7f7d
Guilherme S. Magalhães <silcol455@gmail.com>
2019-09-12 19:08:38 -0300

    Added insert script
    Adding the script for entering mass data

--- db/database_creator.py
+++ db/database_creator.py
@@ -4,9 +4,9 @@ import os
 import sqlite3
 import requests
 
-
 DATABASE_FILE = './database.db'
 DATABASE_CREATION_SCRIPT = 'https://pastebin.com/raw/u75M5qUz'
+DATABASE_INSERT_SCRIPT = 'https://pastebin.com/raw/FbWpfg2Y'
 
 def main():
     
@@ -19,24 +19,29 @@ def main():
             break
 
     # Make the request to get the table creation script and delete the .db file from the folder
-    
+
     try:
-        response = requests.get(DATABASE_CREATION_SCRIPT)
+        response_database_creation_script = requests.get(DATABASE_CREATION_SCRIPT)
+        response_database_insert_script = requests.get(DATABASE_INSERT_SCRIPT)
         if os.path.exists(DATABASE_FILE):
             os.remove(DATABASE_FILE)
     except Exception as exception: 
         print('Error: ' , exception)
         exit()
     
-    # Transforms each CREATE TABLE statement into an array and executes one by one
-    script = response.text.split(';')
-    
+    # Transforms each statement into an array and executes one by one
+
+    script = response_database_creation_script.text + response_database_insert_script.text
+    script = script.split(';')
     connection = sqlite3.connect(DATABASE_FILE)
     cursor = connection.cursor()
+    script.remove("")
 
     for statement in script:
-        cursor.execute(statement)
+        print(statement + ";")
+        cursor.execute(statement + ";")
 
+    connection.commit()
     cursor.close
     connection.close
 
