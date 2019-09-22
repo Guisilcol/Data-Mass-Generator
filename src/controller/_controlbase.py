3cc910d5c1fd1e132bddf28d0cba65596b07246f
Hermes Passer <dsldouglas@hotmail.com>
2019-09-22 09:33:03 -0300

    but fix on controbase's tempfile writing

--- src/controller/_controlbase.py
+++ src/controller/_controlbase.py
@@ -1,13 +1,22 @@
 # Base class from generators, it saves the chunks of data into files
-import tempfile
+from tempfile import TemporaryFile
 
 
 class ControlBase:
 	def __init__(self):
 		self.rows = []
-		self._temp_file = tempfile.TemporaryFile(delete=False)
+		self._temp_file = TemporaryFile('w+', delete=False)
+
+	def __del__(self):
+		import os
+		if not self._temp_file.closed:
+			self._temp_file.close()
+		os.unlink(self._temp_file.name)
 
 	def serialize(self):
-		for item in self.rows:
-				self._temp_file .write(bytes(item))
+		if self._temp_file.closed:
+			return
+		self._temp_file.writelines([f"{str(item)}\n" for item in self.rows])
+		self._temp_file.close()
+
 
